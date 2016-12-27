e24d1ae6fa4ca92d9bbd39ae5797278d76134951
Michael Seemann <seemann@mseemann.de>
2016-12-27 22:21:56 +0100

    working example with angular 2.4.1; angular-cli beta 24 and angular2-mdl 2.11.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { MdlModule } from 'angular2-mdl';
 
 @NgModule({
   imports: [
     HttpModule,
     AppRoutingModule,
+    MdlModule
   ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ],
